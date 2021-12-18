97f250a9d05d03251020aae3e80f57275e7ad841
Makurado Hizato <51111242+tak3488@users.noreply.github.com>
2021-12-18 15:43:03 +0900

    schema変更

---
+++ migrations/20211212142450_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" INTEGER NOT NULL,
+    "blogId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "publishedAt" DATETIME NOT NULL,
+    "content" TEXT NOT NULL,
+    CONSTRAINT "Article_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "Blog" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Blog_id_key" ON "Blog"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Article_id_key" ON "Article"("id");
